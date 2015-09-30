bdbe8d5bb3861bf740f16276f25363cb578b6670
Phals <phals.slahp@gmail.com>
2015-09-30 12:19:40 -0300

    Tempo.
    Controle do tempo.

--- numero_perfeito.py
+++ numero_perfeito.py
@@ -2,20 +2,24 @@
 
 # Numeros perfeitos
 
+import time
+
 n = 2
 
+ini = time.clock()
+
 while 1:
-    
+
     n = n + 1
 
     controle = 0
 
     for i in range(1,n):
-
         if n%i == 0:
-
             controle = controle + i
-
+		
     if controle == n:
+        print(n, 'é um numero perfeito => Tempo programa:', (time.clock())-ini)
 
-        print(n, "\té um numero perfeito")
+    if n%1000 == 0:
+        print('Numero: ', n, 'Tempo programa: ', (time.clock())-ini)
